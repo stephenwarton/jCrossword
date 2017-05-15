ff10b1992b5223ab842cf70c55fe85a2be32e284
Stephen Warton <stephen.warton@gmail.com>
2017-05-15 15:03:52 -0600

    added firebase deployment files

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-$('.lead').append('<h1>HELLO</h1>');
+const API_URL = 'http://jservice.io/api/random?count=10';
+let answerArray = [];
+$(appReady);
+
+function appReady(){
+  $.get(API_URL)
+    .then(getAnswerArray);
+}
+
+function getAnswerArray(clues){
+  for(let object of clues){
+    let answerObject = {};
+    let cleanedAnswer = cleanAnswer(object.answer);
+    answerObject[cleanedAnswer] = object.question + ' (' +object.category.title + ')';
+    answerArray.push(answerObject);
+  }
+  console.log(answerArray);
+}
+
+function cleanAnswer(answerString){
+  let newAnswer = answerString.toLowerCase();
+  newAnswer = newAnswer.replace(/\s+/g, '');
+  return newAnswer;
+}
